7e7b0f25646f2563645af8377c6796ef6970c05d
etridgely <5757423+etridgely@users.noreply.github.com>
2019-11-24 20:19:47 -0800

    finished resource-router

---
+++ resources/resource-router.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const Resources = require("../resources/resource-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Resources.get()
+    .then(resources => {
+      res.status(200).json(resources);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error fetching resources" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Resources.getById(id)
+    .then(resource => {
+      if (!resource[0]) {
+        res.status(404).json({ message: "Invalid ID" });
+      } else {
+        res.status(200).json(resource);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error fetching resource" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const resource = req.body;
+
+  Resources.add(resource)
+    .then(count => {
+      res.status(201).json(count);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error adding resource" });
+    });
+});
+
+module.exports = router;
